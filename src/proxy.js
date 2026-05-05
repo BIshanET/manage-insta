d4792a5747c3d9d8bd0f6bf975d101f83a91695c
BIshanET <bishank.encircle@gmail.com>
2026-05-06 00:33:18 +0530

    debugging

--- src/proxy.js
+++ src/proxy.js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth"
 import { authConfig } from "./auth.config"
 
-export default NextAuth(authConfig).auth
+export default NextAuth({
+  ...authConfig,
+  secret: process.env.AUTH_SECRET,
+}).auth
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
